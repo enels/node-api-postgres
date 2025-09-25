04c669fdc510334b2413ae9e412ed87434d29e23
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-09-25 14:03:21 +0100

    add PUT method to update name and email

--- index.js
+++ index.js
@@ -5,19 +5,21 @@ const app = express();
 const port = 3000;
 
 
-app.use(bodyParser.json());
-app.use(
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+/*app.use(
 	bodyParser.urlencoded({
 
 		extended: true,
 	})
 );
-
+*/
 
 // routes
 app.get('/', queries.home);
 app.get('/users', queries.getUsers);
 app.get('/users/:id', queries.getUser);
+app.post('/users', queries.createUsers);
 
 
 // port listening 
--- queries.js
+++ queries.js
@@ -1,11 +1,14 @@
 const pool = require('./dbconnect')
 
 
+/* GET: / */
 const home = (request, response) => {
 	
 	response.status(200).json({info: 'Node.js, Express.js, PostgreSQL'});
 };
 
+
+/* GET: /users */
 const getUsers = (request, response) => {
 	
 	pool.query('SELECT * FROM users', (error, results) => {
@@ -19,6 +22,7 @@ const getUsers = (request, response) => {
 	});
 };
 
+/* GET: /users/:id */
 const getUser = (request, response) => {
 
 	// get the user parameter from the url
@@ -35,5 +39,39 @@ const getUser = (request, response) => {
 
 };
 
+/* POST: /users */
+const createUsers = (request, resposne) => {
 
-module.exports = {home, getUsers, getUser}
+	// get the user parameters from the url
+	const {name, email} = request.body;
+
+	pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+
+		if (error) {
+
+			throw error;
+		}
+
+		resposne.status(201).json({info: "User Created"});
+	});
+};
+
+
+/* PUT: /users/:id */
+const updateUsers = (request, response) => {
+
+	const user_id = parseInt(request.params.id);
+	const {name, email} = request.body;
+
+	pool.query('UPDATE user_id SET name = $2, email = $3 WHERE id = $3', [$name, $email, $user_id], (error, results) => {
+
+		if (error) {
+			throw error
+		}
+		
+		response.status(201).send(`User modified with ID: ${user_id}`);
+	});
+};
+
+
+module.exports = {home, getUsers, getUser, createUsers}
